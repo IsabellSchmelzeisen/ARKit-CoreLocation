2cfd4e3052ad34ffc4e55bb390117d37b56e7a7f
Isabell Schmelzeisen <isabell.schmelzeisen@deutschebahn.com>
2017-09-22 15:16:04 +0200

    create framework::;

---
+++ ARKIT+CoreLocationFramework/ARKIT+CoreLocationFramework.h
@@ -0,0 +1,19 @@
+//
+//  ARKIT+CoreLocationFramework.h
+//  ARKIT+CoreLocationFramework
+//
+//  Created by Isabell Schmelzeisen on 22.09.17.
+//  Copyright © 2017 Project Dent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKIT+CoreLocationFramework.
+FOUNDATION_EXPORT double ARKIT_CoreLocationFrameworkVersionNumber;
+
+//! Project version string for ARKIT+CoreLocationFramework.
+FOUNDATION_EXPORT const unsigned char ARKIT_CoreLocationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKIT_CoreLocationFramework/PublicHeader.h>
+
+
